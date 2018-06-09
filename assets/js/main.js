915351a68ff5e2848937b052ee78d16f4e1e20f2
bestsuperweb <bestsuperweb@gmail.com>
2018-06-09 14:14:05 -0400

    add js

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+jQuery(document).ready(function($) {
+
+	$(window).scroll(function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		var top = $(this).scrollTop();
+		if( top > 50){
+			$('.navbar-fixed-top').fadeOut(300);
+		}else{
+			$('.navbar-fixed-top').fadeIn(300);
+		}
+	});
+
+
+	function triggerClick(elem) {
+		$(elem).click();
+	}
+	var $progressWizard = $('.stepper'),
+		$tab_active,
+		$tab_prev,
+		$tab_next,
+		$btn_prev = $progressWizard.find('.prev-step'),
+		$btn_next = $progressWizard.find('.next-step'),
+		$tab_toggle = $progressWizard.find('[data-toggle="tab"]'),
+		$tooltips = $progressWizard.find('[data-toggle="tab"][title]');
+
+        // To do:
+        // Disable User select drop-down after first step.
+        // Add support for payment type switching.
+
+        //Initialize tooltips
+        $tooltips.tooltip();
+
+        //Wizard
+        $tab_toggle.on('show.bs.tab', function(e) {
+        	var $target = $(e.target);
+
+        	if (!$target.parent().hasClass('active, disabled')) {
+        		$target.parent().prev().removeClass('disabled').addClass('completed');
+        	}
+        	if ($target.parent().hasClass('disabled')) {
+        		return false;
+        	}
+        });
+
+        // $tab_toggle.on('click', function(event) {
+        //     event.preventDefault();
+        //     event.stopPropagation();
+        //     return false;
+        // });
+
+        $btn_next.on('click', function() {
+        	$tab_active = $progressWizard.find('.active');
+
+        	$tab_active.next().removeClass('disabled');
+
+        	$tab_next = $tab_active.next().find('a[data-toggle="tab"]');
+        	triggerClick($tab_next);
+
+        });
+        $btn_prev.click(function() {
+        	$tab_active = $progressWizard.find('.active');
+        	$tab_prev = $tab_active.prev().find('a[data-toggle="tab"]');
+        	triggerClick($tab_prev);
+        });
+
+        $("#wins-slider").slider({
+        	formatter: function(value) {
+				return value + ' WINS';
+			}
+        });
+
+
+
+});
